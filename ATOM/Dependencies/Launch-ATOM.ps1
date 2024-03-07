c1f4a70c5f48ac30f0b1d54fa95ce394ef4acc1a
SkylerWallace <northernlights1997@gmail.com>
2024-03-06 20:36:17 -0500

    Launch-ATOM & Neutron improvements
    Launch-ATOM
    - Vastly improved logic and error-handling
    
    Neutron
    - Added Test-Predicate function to simplify predicate checks

--- ATOM/Dependencies/Launch-ATOM.ps1
+++ ATOM/Dependencies/Launch-ATOM.ps1
@@ -1,49 +1,110 @@
-# Launch ATOM if already downloaded to temp
+# Declare function to launch ATOM
 $atomBat = Join-Path $env:TEMP "ATOM\ATOM.bat"
-if (Test-Path $atomBat) {
-	Start-Process $atomBat
-	exit
+function Launch-ATOM {
+	try {
+		Start-Process $atomBat
+		exit
+	} catch {
+		Write-Host "`nFailed to launch ATOM!"
+		Write-Host "Try to run launch line again"
+		Write-Host "Or try to manually launch at the following directory:"
+		Write-Host "$atomBat`n"
+		return
+	}
 }
 
-# If internet connected, download latest ATOM to temp
+# Check internet connection
 $internetConnected = (Get-NetConnectionProfile | Where-Object { $_.NetworkCategory -eq 'Public' -or $_.NetworkCategory -eq 'Private' }) -ne $null
-if ($internetConnected) {
-	Write-Host "Downloading ATOM..."
-	
-	try {
-		$atomUrl = "https://github.com/SkylerWallace/ATOM/archive/refs/heads/main.zip"
-		$atomDestination = Join-Path $env:TEMP "ATOM-main.zip"
-		$progressPreference = "SilentlyContinue"
-		Invoke-WebRequest -Uri $atomUrl -OutFile $atomDestination
-	} catch {
-		Write-Host "`nUnable to download latest ATOM."
-		Write-Host "Potential issue with ATOM host or internet connection."
-		Write-Host "Aborting update process..."
-		Start-Sleep -Seconds 5
-		exit	
+
+# Suppress progress bar to prioritize download speed
+$progressPreference = "SilentlyContinue"
+
+# Check if ATOM is already downloaded to temp
+$atomDetected = Test-Path $atomBat
+
+# Get hashes of existing ATOM and from latest ATOM online
+if (!$internetConnected) {
+	Write-Host "`nNo internet connection detected."
+	$failState = $true
+} elseif ($atomDetected -and $internetConnected) {
+	# Get local hash
+	$hashPath = Join-Path $env:TEMP "ATOM\ATOM\Dependencies\Settings\hash.txt"
+	if (Test-Path $hashPath) {
+		$localHash = (Get-Content -Path $hashPath).TrimEnd()
+	} else {
+		Write-Host "Failed to get hash from the following path:"
+		Write-Host $hashPath
+		$failState = $true
 	}
 	
-	Write-Host "ATOM downloaded!`n"
-} else {
-	Write-Host "`nNo internet connection detected."
-	Write-Host "Aborting update process..."
-	Start-Sleep -Seconds 5
-	exit
+	# Get online hash
+	$hashUrl = "https://raw.githubusercontent.com/SkylerWallace/ATOM/main/ATOM/Dependencies/Settings/hash.txt"
+	try {
+		$onlineHash = (Invoke-WebRequest -Uri $hashUrl).Content.TrimEnd()
+	} catch {
+		Write-Host "Failed to get hash from the following URL:"
+		Write-Host $hashUrl
+		$failState = $true
+	}
 }
 
+# Launch existing ATOM or close script if unable to compare hashes
+if ($failState) {
+	Write-Host "`nUnable to check for new ATOM version."
+	
+	if ($atomDetected) {
+		Write-Host "Launching local ATOM without updating..."
+		Start-Sleep -Seconds 5
+		Launch-ATOM
+	}
+	
+	Write-Host "`nProcess aborted.`n"
+	return
+}
+
+# If currentHash and onlineHash are declared and equal, launch local ATOM & exit
+if (($localHash -and $onlineHash) -and ($localHash -eq $onlineHash)) {
+	Launch-ATOM
+}
+
+# Download latest ATOM to temp
+Write-Host "Downloading ATOM..."
+
+$atomUrl = "https://github.com/SkylerWallace/ATOM/archive/refs/heads/main.zip"
+$atomDestination = Join-Path $env:TEMP "ATOM-main.zip"
+
+try {
+	Invoke-WebRequest -Uri $atomUrl -OutFile $atomDestination
+} catch {
+	Write-Host "`nUnable to download latest ATOM."
+	Write-Host "Potential issue with ATOM download URL:"
+	Write-Host $atomUrl
+	Write-Host "`nProcess aborted.`n"
+	return
+}
+
+Write-Host "ATOM downloaded!`n"
 
 # Unzip ATOM
 $atomUnzipped = Join-Path $env:TEMP "ATOM-main"
 if (Test-Path $atomUnzipped) { Remove-Item $atomUnzipped -Recurse }
 Expand-Archive -Path $atomDestination -DestinationPath $atomUnzipped
 
+# Remove existing ATOM from temp if detected
+$atomPath = Join-Path $env:TEMP "ATOM"
+if (Test-Path $atomPath) {
+	Get-Process | Where-Object { $_.ProcessName -in "powershell", "pwsh" } | Where-Object { $_.MainWindowTitle -like "ATOM*" } | Stop-Process -Force | Wait-Process
+	Start-Sleep -Seconds 3
+	Remove-Item $atomPath -Force -Recurse
+}
+
 # Copy files
 $atomSubDir = Join-Path $atomUnzipped "ATOM-main"
-$atomPath = Join-Path $env:TEMP "ATOM"
 Copy-Item -Path $atomSubDir -Destination $atomPath -Force -Recurse
 
 # Cleanup
 Remove-Item -Path $atomDestination -Force
 Remove-Item -Path $atomUnzipped -Recurse
 
-Start-Process $atomBat
+# Final launch
+Launch-ATOM
--- ATOM/Dependencies/Neutron/Customizations.ps1
+++ ATOM/Dependencies/Neutron/Customizations.ps1
@@ -1,13 +1,23 @@
+function Test-Predicate {
+    param (
+        [string]$path,
+        [string]$name,
+		[int]$value,
+        [int]$minWinVer = 0,
+        [int]$minWinBuild = 0
+    )
+	
+    try {
+        (Get-ItemPropertyValue -Path $path -Name $name) -ne $value
+    } catch {
+		($winVer -ge $minWinVer) -and ($winBuild -ge $minWinBuild)
+    }
+}
+
 $customizations = [ordered]@{
 	"Dark Mode" = @{
 		tooltip = "Yes"
-		predicate = {
-			try {
-				(Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme") -ne 0
-			} catch {
-				($winVer -ge 10) -and ($winBuild -ge 18282)
-			}
-		}
+		predicate = { Test-Predicate -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0 -MinWinVer 10 -MinWinBuild 18282 }
 		scriptblock = {
 			Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Type "DWord" -Value "0"
 			Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Type "DWord" -Value "0"
@@ -33,9 +43,7 @@ $customizations = [ordered]@{
 	
 	"Disable Mouse Acceleration" = @{
 		tooltip		= "Important tweak for some gamers"
-		predicate	= {
-			(Get-ItemPropertyValue -Path "HKCU:\Control Panel\Mouse" -Name "MouseSpeed") -ne 0
-		}
+		predicate	= { Test-Predicate -Path "HKCU:\Control Panel\Mouse" -Name "MouseSpeed" -Value 0 }
 		scriptblock	= {
 			Set-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name "MouseSpeed" -Type "String" -Value "0"
 			Set-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name "MouseThreshold1" -Type "String" -Value "0"
@@ -46,13 +54,7 @@ $customizations = [ordered]@{
 	
 	"Disable Notifications" = @{
 		tooltip		= "This is system-wide, will also disable Windows Security notifs"
-		predicate	= {
-			try {
-				(Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled") -ne 0
-			} catch {
-				($winVer -ge 10) -and ($winBuild -ge 14328)
-			}
-		}
+		predicate	= { Test-Predicate -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -Value 0 -MinWinVer 10 -MinWinBuild 14328 }
 		scriptblock	= {
 			Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -Type "DWord" -Value "0"
 			Write-OutputBox "- Disabled Notifications"
@@ -61,13 +63,7 @@ $customizations = [ordered]@{
 	
 	"Taskbar - Disable Chat" = @{
 		tooltip		= "Disable Chat icon in taskbar"
-		predicate	= {
-			try {
-				(Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarMn") -ne 0
-			} catch {
-				($winVer -ge 11) -and ($winBuild -ge 22000)
-			}
-		}
+		predicate	= { Test-Predicate -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarMn" -Value 0 -MinWinVer 11 -MinWinBuild 22000 }
 		scriptblock	= {
 			Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarMn" -Type "DWord" -Value "0"
 			Write-OutputBox "- Disabled Chat button"
@@ -76,13 +72,7 @@ $customizations = [ordered]@{
 	
 	"Taskbar - Disable Copilot" = @{
 		tooltip		= "Disable Copilot icon in taskbar"
-		predicate	= {
-			try {
-				(Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowCopilotButton") -ne 0
-			} catch {
-				($winVer -ge 11)
-			}
-		}
+		predicate	= { Test-Predicate -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowCopilotButton" -Value 0 -MinWinVer 11 }
 		scriptblock	= {
 			# Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\WindowsCopilot" -Name "TurnOffWindowsCopilot" -Type "DWord" -Value "1"
 			Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowCopilotButton" -Type "DWord" -Value "0"
@@ -92,13 +82,7 @@ $customizations = [ordered]@{
 	
 	"Taskbar - Disable Search" = @{
 		tooltip		= "Set search bar to hidden in taskbar"
-		predicate	= {
-			try {
-				(Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode") -ne 0
-			} catch {
-				($winVer -ge 10)
-			}
-		}
+		predicate	= { Test-Predicate -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Value 0 -MinWinVer 10 }
 		scriptblock	= {
 			Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Type "DWord" -Value "0"
 			Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarModeCache" -Type "DWord" -Value "1"
@@ -108,13 +92,7 @@ $customizations = [ordered]@{
 	
 	"Taskbar - Disable Task View" = @{
 		tooltip		= "Disable Task View icon in taskbar"
-		predicate	= {
-			try {
-				(Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton") -ne 0
-			} catch {
-				($winVer -ge 10)
-			}
-		}
+		predicate	= { Test-Predicate -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value 0 -MinWinVer 10 }
 		scriptblock	= {
 			Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Type "DWord" -Value "0"
 			Write-OutputBox "- Disabled Task View button"
@@ -123,13 +101,7 @@ $customizations = [ordered]@{
 	
 	"Taskbar - Disable Widgets" = @{
 		tooltip		= "Disable Widgets icon in taskbar"
-		predicate	= {
-			try {
-				(Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa") -ne 0
-			} catch {
-				($winVer -ge 10) -and ($winBuild -ge 22000)
-			}
-		}
+		predicate	= { Test-Predicate -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -Value 0 -MinWinVer 11 -MinWinBuild 22000 }
 		scriptblock	= {
 			Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -Type "DWord" -Value "0"
 			Write-OutputBox "- Disabled Widgets"
@@ -138,13 +110,7 @@ $customizations = [ordered]@{
 	
 	"Taskbar - Left Align" = @{
 		tooltip		= "Left-align taskbar icons in Windows 11"
-		predicate	= {
-			try {
-				(Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl") -ne 0
-			} catch {
-				($winVer -ge 11) -and ($winBuild -ge 22000)
-			}
-		}
+		predicate	= { Test-Predicate -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Value 0 -MinWinVer 11 -MinWinBuild 22000 }
 		scriptblock	= {
 			Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Type "DWord" -Value "0"
 			Write-OutputBox "- Taskbar left-aligned"
@@ -153,9 +119,7 @@ $customizations = [ordered]@{
 	
 	"Update Apps" = @{
 		tooltip		= "Update all eligible apps with 'winget upgrade --all'"
-		predicate	= {
-			($winVer -ge 10) -and ($winBuild -ge 17763)
-		}
+		predicate	= { Test-Predicate -MinWinVer 10 -MinWinBuild 17763 }
 		scriptblock	= {
 			winget upgrade --all --accept-package-agreements --accept-source-agreements --force --silent
 			Write-OutputBox "- Installed Microsoft Store app updates"
