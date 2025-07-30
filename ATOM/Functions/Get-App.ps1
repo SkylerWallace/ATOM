4ce1bb569e18e5fa98c16165223d43e1adbc54f4
SkylerWallace <northernlights1997@gmail.com>
2025-07-30 00:22:09 -0400

    App functions
    - Added Get-App and Remove-App functions

---
+++ ATOM/Functions/Get-App.ps1
@@ -0,0 +1,76 @@
+function Get-App {
+	<#
+	.SYNOPSIS
+	Retrieves properties of applications installed on the system.
+
+	.DESCRIPTION
+	The `Get-App` function scans the registry for uninstall information 
+	of software installed in both 64-bit and 32-bit program areas, as well as user-specific 
+	installations. It returns details including the display name, uninstall string, 
+	and a quiet uninstall string if available.
+
+	.PARAMETER DisplayName
+	Specifies the name or part of the name of the software to search for. 
+	This function supports pipeline input for this parameter.
+
+	.PARAMETER Scope
+	Specifies whether to search for 64-bit, 32-bit, and/or user applications.
+    Can be `All`, `User`, `x64`, or `x86`. Default is `All`.
+
+	.EXAMPLE
+	Get-App
+    Retrieves all installed applications.
+
+    .EXAMPLE
+    Get-App -Name 'Windows' -Scope x64
+    Retrieves all 64-bit applications that match the display name 'Windows'.
+
+    .EXAMPLE
+    'Microsoft', 'Windows' | Get-App
+    Retrieves all applications that match the display names 'Microsoft' and 'Windows'.
+
+	.INPUTS
+	[System.String]
+    Accepts an array of strings representing software names to search for.
+
+    .OUTPUTS
+    [PsCustomObject]
+    Returns a list of custom objects representing installed applications. Each object contains the following properties:
+    - DisplayName          : The name of the application as shown in Programs and Features.
+    - DisplayVersion       : The version number of the application.
+    - Publisher            : The software publisher.
+    - EstimatedSize        : The approximate size of the application, in KB.
+    - PsPath               : The full registry path of the uninstall entry.
+    - UninstallString      : The command used to uninstall the application.
+    - QuietUninstallString : The command used to silently uninstall the application, if available.
+
+	.NOTES
+	Author: Skyler Wallace
+	#>
+
+    [CmdletBinding()]
+
+    param (
+        [Alias('Name')][Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)]
+        [string[]]$displayName = '',
+        [ValidateSet('All', 'User', 'x64', 'x86')]
+        [string[]]$scope = 'All'
+    )
+
+    $uninstallPaths = switch ($scope) {
+        { $_ -in 'User', 'All' } { 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall' }
+        { $_ -in  'x64', 'All' } { 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall' }
+        { $_ -in  'x86', 'All' } { 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall' }
+    }
+    
+    $uninstallKeys = Get-ChildItem $uninstallPaths | ForEach-Object {
+        Get-ItemProperty $_.PsPath | Where-Object { $_.DisplayName -and ($_.UninstallString -or $_.QuietUninstallString) }
+    }
+
+    $apps = foreach ($n in $displayName) {
+        $uninstallKeys | Where-Object { $_.DisplayName -match $n } |
+        Select-Object DisplayName, DisplayVersion, Publisher, EstimatedSize, PsPath, UninstallString, QuietUninstallString
+    }
+
+    $apps | Sort-Object PsPath -Unique
+}
---
+++ ATOM/Functions/Remove-App.ps1
@@ -0,0 +1,84 @@
+function Remove-App {
+	<#
+	.SYNOPSIS
+	Uninstalls an application from the system.
+
+	.DESCRIPTION
+	The `Remove-App` function uninstalls an application using its uninstall or quiet uninstall string.
+    Supports MSI and EXE-based uninstallers, handling path quoting and argument parsing automatically.
+
+	.PARAMETER App
+    Specifies an object from Get-App.
+
+	.EXAMPLE
+	Get-App 'Microsoft Edge' | Remove-App
+    Uninstalls Microsoft Edge.
+
+	.INPUTS
+	[PsCustomObject]
+    Accepts output from Get-App which has the following properties:
+    - DisplayName          : The name of the application as shown in Programs and Features.
+    - DisplayVersion       : The version number of the application.
+    - Publisher            : The software publisher.
+    - EstimatedSize        : The approximate size of the application, in KB.
+    - PsPath               : The full registry path of the uninstall entry.
+    - UninstallString      : The command used to uninstall the application.
+    - QuietUninstallString : The command used to silently uninstall the application, if available.
+
+    .OUTPUTS
+    None. This function does not produce output to the pipeline.
+
+	.NOTES
+	Author: Skyler Wallace
+	#>
+
+    [CmdletBinding()]
+
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [psCustomObject]$app
+    )
+
+    process {
+        $uninstallString = @(
+            if ($app.QuietUninstallString) { $app.QuietUninstallString }
+            elseif ($app.UninstallString) { $app.UninstallString }
+        ) -replace '(?<!")([a-zA-Z]:\\[^"]+\.(bat|cmd|exe|msi))(?!")', '"$1"'
+
+        $uninstallParams = @{
+            Wait		= $true
+            PassThru	= $true
+        }
+
+        # Check if uninstaller uses msiexec with regex
+        $msiMatch = [regex]::Match($uninstallString, '\/[IX]\{[^}]*\}')
+        $exeMatch = [regex]::Match($uninstallString, '"[^"]+"')
+
+        # If msiexec uninstaller
+        if ($msiMatch.Success) {
+            $uninstallParams.FilePath     = ($uninstallString -replace $msiMatch.Value, '').Trim()
+            $uninstallParams.ArgumentList = 
+                if ($uninstallParams.FilePath -match '/qn') { $msiMatch.Value } 
+                else { "$($msiMatch.Value) /qn" }
+        # If .cmd/.bat/.exe uninstaller
+        } elseif ($exeMatch.Success) {
+            $uninstallParams.FilePath     = $exeMatch.Value
+            $uninstallParams.ArgumentList = ($uninstallString -replace [regex]::Escape($exeMatch.Value), '').Trim()
+        }
+
+        Write-Verbose "Uninstalling $($app.DisplayName)"
+        $process = Start-Process @uninstallParams
+
+        if (!(Test-Path $app.PsPath)) {
+            Write-Verbose "- Uninstalled"
+            return
+        }
+
+        Write-Error (
+            "Failed to uninstall.",
+            "DisplayName     : $($app.DisplayName)",
+            "UninstallString : $($uninstallParams.FilePath) $($uninstallParams.ArgumentList)",
+            "ExitCode        : $($process.ExitCode)" -join "`n"
+        )
+    }
+}
