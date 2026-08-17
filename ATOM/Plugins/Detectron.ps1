10de2964184e00670429e007c204c324a0ce79c2
SkylerWallace <northernlights1997@gmail.com>
2026-08-17 15:55:50 -0400

    Detectron consolidation

--- ATOM/Plugins/Detectron.ps1
+++ ATOM/Plugins/Detectron.ps1
@@ -91,10 +91,375 @@ Set-VectorIcon -ForegroundResource primaryText -ResourceMappings @{
 }
 
 # Construct panels
-. $detectronPanels\Panel-Notifications.ps1
-. $detectronPanels\Panel-Optimizations.ps1
-. $detectronPanels\Panel-Programs.ps1
-. $detectronPanels\Panel-Apps.ps1
+# Notification panel
+Add-Type -AssemblyName System.Windows.Forms
+
+$whitelistSites = @(
+    ("www.calendar.google.com"),
+    ("www.facebook.com"),
+    ("www.mail.google.com"),
+    ("www.outlook.live.com"),
+    ("www.meet.google.com"),
+    ("www.netflix.com"),
+    ("www.youtube.com"),
+    ("www.zoom.us")
+)
+
+function Check-Notifications {
+    $textBlock = New-Object System.Windows.Controls.TextBlock
+    $textBlock.Foreground = $surfaceText
+    $textBlock.Margin = "5"
+    $stackPanel.Children.Add($textBlock) | Out-Null
+    
+    $browserButton = New-Object System.Windows.Controls.Button
+    $browserButton.Background = $accentBrush
+    $browserButton.Foreground = $accentText
+    $browserButton.Tag = $browserName
+    $browserButton.Content = $buttonContent
+    $browserButton.Tooltip = $buttonTooltip
+    $browserButton.Style = $window.Resources["RoundedButton"]
+    $browserButton.Margin = "5"
+    $browserButton.Tag = @{ "Name" = $browserName; "URL" = $notificationsUrl }
+    $stackPanel.Children.Add($browserButton) | Out-Null
+    $browserButton.Add_Click({
+        $buttonTag = $_.Source.Tag
+        [System.Windows.Forms.Clipboard]::SetText($buttonTag["URL"])
+        start $buttonTag["Name"]
+    })
+    
+    $preferencesFilePaths = @("$userDataPath\Default\Preferences")
+    $profiles = Get-ChildItem "$userDataPath" | Where-Object {$_.PSIsContainer -and $_.Name.StartsWith("Profile")}
+    foreach ($profile in $profiles) {
+        $preferencesFilePaths += "$($profile.FullName)\Preferences"
+    }
+    $totalSiteCounter = 0
+    $totalWhitelistCounter = 0
+    
+    $textBlock.Text = $browserName
+    foreach ($preferencesFilePath in $preferencesFilePaths) {
+        $preferences = Get-Content $preferencesFilePath -Raw
+        $userName = if ($browserName -eq "Chrome") { ($preferences | ConvertFrom-Json).account_info[0].full_name }
+                    elseif ($browserName -eq "MSEdge") { [regex]::Match($preferences, '"edge_account_first_name":"([^"]+)"').Groups[1].Value }
+        
+        $notifications = ($preferences | Select-String -Pattern '(?<="notifications":{)(.*?)(?=},"password_protection":)').Matches.Value | Out-String
+        $urls = [regex]::Matches($notifications, '(?:www\.)?[a-z]+\.[a-z]+(?:\.[a-z]+)*') | ForEach-Object { $_.Value }
+        $whitelistCounter = 0
+        foreach ($url in $urls) {
+            if ($whitelistSites -contains $url) {
+                $whitelistCounter++
+                $totalWhitelistCounter++
+            } else {
+                $totalSiteCounter++
+            }
+        }
+        $totalSiteCounter += $whitelistCounter
+        
+        $textBlock.Text += "`n- $($username): $whitelistCounter / $($urls.Count) sites whitelisted"
+    }
+}
+
+$browserTextBlock = New-Object System.Windows.Controls.TextBlock
+$browserTextBlock.Text = "Browser Notifications"
+$browserTextBlock.FontWeight = "Bold"
+$browserTextBlock.Foreground = $backgroundText
+$browserTextBlock.Margin = "10,5,0,0"
+$uninstallPanel.Children.Add($browserTextBlock) | Out-Null
+
+$border = New-Object System.Windows.Controls.Border
+$border.Style = $window.Resources["CustomBorder"]
+$border.Margin = "10,10,0,10"
+$border.Padding = "5"
+$uninstallPanel.Children.Add($border) | Out-Null
+
+$stackPanel = New-Object System.Windows.Controls.StackPanel
+$border.Child = $stackPanel
+
+# Edge
+$browserPath = "C:\Program Files*\Microsoft\Edge"
+$userDataPath = "$env:LOCALAPPDATA\Microsoft\Edge\User Data"
+$preferencesPath = "$userDataPath\*\Preferences"
+
+if ((Test-Path $browserPath) -And (Test-Path $preferencesPath)) {
+    $buttonContent = "Launch Edge"
+    $buttonTooltip = "- Launches Edge`n- Adds notification URL to clipboard"
+    
+    $browserName = "MSEdge"
+    $notificationsUrl = "edge://settings/content/notifications"
+
+    Check-Notifications
+}
+
+# Chrome
+$browserPath = "C:\Program Files*\Google\Chrome"
+$userDataPath = "$env:LOCALAPPDATA\Google\Chrome\User Data"
+$preferencesPath = "$userDataPath\*\Preferences"
+
+if ((Test-Path $browserPath) -And (Test-Path $preferencesPath)) {
+    $buttonContent = "Launch Chrome"
+    $buttonTooltip = "- Launches Chrome`n- Adds notification URL to clipboard"
+    
+    $browserName = "Chrome"
+    $notificationsUrl = "chrome://settings/content/notifications"
+
+    Check-Notifications
+}
+
+# Optimizations panel
+$optimizationsCheckbox = New-Object System.Windows.Controls.CheckBox
+$optimizationsCheckbox.Content = "Optimizations"
+$optimizationsCheckbox.ToolTip = "Check all optimizations."
+$optimizationsCheckbox.FontWeight = "Bold"
+$optimizationsCheckbox.Foreground = $backgroundText
+$optimizationsCheckbox.Margin = "10,5,0,0"
+$uninstallPanel.Children.Add($optimizationsCheckbox) | Out-Null
+
+$optimizationsListBox = New-Object System.Windows.Controls.ListBox
+$optimizationsListBox.Background = $surfaceColor
+$optimizationsListBox.Foreground = $surfaceText
+$optimizationsListBox.BorderThickness = 0
+$optimizationsListBox.Margin = "10,5,0,5"
+$optimizationsListBox.Style = $window.Resources["CustomListBoxStyle"]
+$uninstallPanel.Children.Add($optimizationsListBox) | Out-Null
+
+Get-ChildItem -Path $detectronOptimizations -Filter *.ps1 | Sort-Object | ForEach-Object {
+    $checkBox = New-Object System.Windows.Controls.CheckBox
+    $checkBox.Content = $_.BaseName
+    $checkBox.Tag = $_.FullName
+    $checkBox.Foreground = $surfaceText
+    $checkBox.VerticalAlignment = [System.Windows.VerticalAlignment]::Center
+    
+    # Add tooltip if first line of script starts with "$tooltip = "
+    $firstLine = Get-Content $_.FullName -First 1
+    if ($firstLine.StartsWith('$tooltip = ')) {
+        Invoke-Expression $firstLine
+        $checkBox.ToolTip = $tooltip
+    }
+    
+    $optimizationsItems = $optimizationsListBox.Items
+    $optimizationsItems.Add($checkBox) | Out-Null
+}
+
+$optimizationsCheckbox.Add_Checked({
+    foreach ($item in $optimizationsItems) {
+        if ($item.IsEnabled) {
+            $item.IsChecked = $true
+        }
+    }
+})
+
+$optimizationsCheckbox.Add_Unchecked({
+    foreach ($item in $optimizationsItems) {
+        if ($item.IsEnabled) {
+            $item.IsChecked = $false
+        }
+    }
+})
+
+# Programs panel
+# Import programs hashtable
+$programsHashtable = Join-Path $detectronPrograms "Programs.ps1"
+. $programsHashtable
+
+# All uninstall keys
+$uninstallPaths = @(
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", # 64-bit programs
+    "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall", # 32-bit programs
+    "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" # User programs
+)
+
+# Store all uninstall keys in single variable
+$uninstallKeys = Get-ChildItem $uninstallPaths | ForEach {
+    Get-ItemProperty $_.PSPath | Where { $_.DisplayName -and $_.UninstallString } | Select DisplayName, PsPath, QuietUninstallString, UninstallString
+}
+
+# Detect programs
+$detectedPrograms = @{}
+foreach ($category in $programs.Keys) {
+    foreach ($program in $programs.$category) {
+        # Detect programs from programs hashtable
+        $detectedProgram = $uninstallKeys | Where { $_.DisplayName -match $program }
+        
+        # Early exit
+        if (!$detectedProgram) { continue }
+        
+        # Add category to hashtable if not detected
+        if (!$detectedPrograms.ContainsKey($category)) {
+            $detectedPrograms.$category = @{}
+        }
+        
+        # Add to detectedPrograms hashtable
+        $detectedProgram | ForEach {
+            $detectedPrograms.$category.($_.DisplayName) = @{
+                DisplayName = $_.DisplayName
+                Key = $_.PsPath
+                UninstallString = $(
+                    if ($_.QuietUninstallString -ne $null) { $_.QuietUninstallString }
+                    else { $_.UninstallString }
+                ) -replace '(?<!")([a-zA-Z]:\\[^"]+\.(exe|msi))(?!")', '"$1"'
+            }
+        }
+    }
+}
+
+# Listboxes hashtable
+$listBoxes = @{}
+
+# Create listboxes w/ checkboxes for detected programs.
+# If no matching programs are detected, this loop simply does nothing;
+# do not return here because this code runs in the script scope.
+foreach ($category in $detectedPrograms.Keys) {
+    # create a TextBlock for the category name and add it to the stackPanel
+    $categoryCheckBox = New-Object System.Windows.Controls.CheckBox
+    $categoryCheckBox.Content = $category
+    $categoryCheckBox.Tag = $category
+    $categoryCheckBox.ToolTip = "Check all $category apps that are safe to remove."
+    $categoryCheckBox.FontWeight = "Bold"
+    $categoryCheckBox.Foreground = $backgroundText
+    $categoryCheckBox.Margin = "10,5,0,0"
+    $uninstallPanel.Children.Add($categoryCheckBox) | Out-Null
+    
+    # create a listBox for the programs in this category
+    $listBox = New-Object System.Windows.Controls.ListBox
+    $listBox.Background = $surfaceBrush
+    $listBox.Foreground = $surfaceText
+    $listBox.BorderThickness = 0
+    $listBox.Margin = "10,5,0,5"
+    $listBox.Style = $window.Resources["CustomListBoxStyle"]
+    $uninstallPanel.Children.Add($listBox) | Out-Null
+    
+    $listBoxes.$category = $listBox
+    
+    $categoryCheckBox.Add_Checked({
+        $currentCategory = $this.Tag
+        foreach ($item in $listBoxes.$currentCategory.Items) {
+            $item.IsChecked = $true
+        }
+    })
+    
+    $categoryCheckBox.Add_Unchecked({
+        $currentCategory = $this.Tag
+        foreach ($item in $listBoxes.$currentCategory.Items) {
+            $item.IsChecked = $false
+        }
+    })
+    
+    # Add programs under the category
+    foreach ($programName in $detectedPrograms.$category.Keys) {
+        $checkBox = New-Object System.Windows.Controls.CheckBox
+        $checkBox.Content = $programName
+        $checkBox.Tag = $programName
+        $checkBox.Foreground = $surfaceText
+        $checkBox.VerticalAlignment = [System.Windows.VerticalAlignment]::Center
+
+        $listBox.Items.Add($checkbox) | Out-Null
+    }
+}
+
+# Apps panel
+# Import $apps hashtable
+$appsHashtable = Join-Path $detectronPrograms "Apps.ps1"
+. $appsHashtable
+
+# Variables needed in foreach loop
+$detectedApps = @()
+$packagesPath = Join-Path $env:LOCALAPPDATA "Packages"
+
+# Iterate through $apps hashtable and search for apps
+foreach ($app in $apps.Keys) {
+    $packageName = $apps[$app]["PackageName"]
+    $publisherId = $apps[$app]["PublisherId"]
+    $appPath = Join-Path $packagesPath "$($packageName)_$($publisherId)"
+    $userDataPath = Join-Path $appPath $apps[$app]["UserData"]
+    
+    # Add app to detectedApps array if detected
+    if (Test-Path $appPath) {
+        $detectedApps += $app
+    }
+    
+    # Add user data detection to hashtable
+    if (($apps[$app]["UserData"] -ne $null) -and (Test-Path $userDataPath)) {
+        $apps[$app].UserDataDetected = $true
+    }
+}
+
+# Create panel for apps
+if ($detectedApps.Count -ge 1) {
+    # Master checkbox
+    $appxCheckbox = New-Object System.Windows.Controls.CheckBox
+    $appxCheckbox.Content = "AppX Bloatware"
+    $appxCheckbox.ToolTip = "Check all AppX apps that are safe to remove."
+    $appxCheckbox.FontWeight = "Bold"
+    $appxCheckbox.Foreground = $backgroundText
+    $appxCheckbox.Margin = "10,5,0,0"
+
+    $appxListBox = New-Object System.Windows.Controls.ListBox
+    $appxListBox.Margin = "10,5,0,5"
+    $appxListBox.Style = $window.Resources["CustomListBoxStyle"]
+    
+    $uninstallPanel.Children.Add($appxCheckbox) | Out-Null
+    $uninstallPanel.Children.Add($appxListBox) | Out-Null
+    
+    # Create individual checkboxes for all detected apps
+    $selectedApps = New-Object System.Collections.ArrayList
+    foreach ($detectedApp in $detectedApps) {
+        $checkBox = New-Object System.Windows.Controls.CheckBox
+        $checkBox.Content = $detectedApp
+        $checkBox.Tag = $detectedApp
+        $checkBox.Foreground = $surfaceText
+        $checkBox.VerticalAlignment = "Center"
+        
+        # Variables to check key booleans
+        $isImportant = $apps[$detectedApp]["Important"] -eq $true
+        $isUserDataDetected = $apps[$detectedApp]["UserDataDetected"] -eq $true
+        
+        # If Important key or UserDataDetected key are true
+        if ($isImportant -or $isUserDataDetected) {
+            $checkBox.ToolTip = "$($detectedApp)`n"
+        }
+        
+        # If Important key is $true
+        if ($isImportant) {
+            $checkBox.Content += " [I]"
+            $checkBox.ToolTip += "[I] Important`n"
+            $checkBox.ToolTip += "Potentially important app.`n"
+            $checkBox.ToolTip += "This app will not be checked by the AppX Bloatware checkbox."
+        }
+        
+        # If UserDataDetected key is $true
+        if ($isUserDataDetected) {
+            $checkBox.Content += " [UD]"
+            if ($checkBox.ToolTip -ne $null) { $checkBox.ToolTip += "`n" }
+            $checkBox.ToolTip += "[UD] User Data`n"
+            $checkBox.ToolTip += "User data detected, user has used app.`n"
+            $checkBox.ToolTip += "This app will not be checked by the AppX Bloatware checkbox."
+        }
+        
+        $appxListBox.Items.Add($checkBox) | Out-Null
+    }
+    
+    # Master checkbox - check event handler
+    $appxCheckbox.Add_Checked({
+        foreach ($item in $appxListBox.Items) {
+            $important = $apps[$item.Tag]["Important"]
+            $userDataDetected = $apps[$item.Tag]["UserDataDetected"]
+            
+            if (($important -ne $true) -and ($userDataDetected -ne $true)) {
+                $item.IsChecked = $true
+                if (!$selectedApps.Contains($item.Tag)) {
+                    $selectedApps.Add($item.Tag) | Out-Null
+                }
+            }
+        }
+    })
+    
+    # Master checkbox - uncheck event handler
+    $appxCheckbox.Add_Unchecked({
+        foreach ($item in $appxListBox.Items) {
+            $item.IsChecked = $false
+            $selectedApps.Remove($item.Tag) | Out-Null
+        }
+    })
+}
 
 0..1 | ForEach-Object { $window.FindName("scrollViewer$_").AddHandler([System.Windows.UIElement]::MouseWheelEvent, [System.Windows.Input.MouseWheelEventHandler]{ param($sender, $e) $sender.ScrollToVerticalOffset($sender.VerticalOffset - $e.Delta) }, $true) }
 $minimizeButton.Add_Click({ $window.WindowState = 'Minimized' })
